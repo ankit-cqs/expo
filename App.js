4ed86bf1b0c77317bdb81580f2952b8008f346c7
Ankit Yadav <67993640+ankit-py-1118@users.noreply.github.com>
2023-06-08 18:35:04 +0530

    Add react-native-material

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button,HStack, Banner } from "@react-native-material/core";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Text>Track My Train</Text>
       <StatusBar style="auto" />
+      <Banner
+    text="There was a problem processing a transaction on your credit card."
+    buttons={
+      <HStack spacing={2}>
+        <Button key="fix-it" variant="text" title="Fix it" compact />
+        <Button key="learn-more" variant="text" title="Learn More" compact />
+      </HStack>
+    }
+  />
+
+      <Button title="Start working" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
